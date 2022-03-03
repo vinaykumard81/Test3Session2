50ca883d56f2a5037edc902e2bbb4ac6d75b700a
vinaykumard81 <97220849+vinaykumard81@users.noreply.github.com>
2022-03-03 08:50:27 +0000

    P1

--- p1final.c
+++ p1final.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+struct _fraction
+{
+  int num,den;
+};
+typedef struct_fraction Fraction;
+int find_gcd(int a, int b)
+{
+  int t;
+.  while (b ! = 0)
+    {
+      t = b;
+      b = a%b;
+      a = t;
+    }
+  return a;}
+
+int main()
+{
+  printf("%d\n",gcd(16,24));
+  return 0;
+}
